6d2ae1bd1a2999224b041afc7b542f444c068747
ankitpmehta1 <127501184+ankitpmehta1@users.noreply.github.com>
2025-05-13 21:36:12 +0530

    Update TBG.py

--- app/TBG.py
+++ app/TBG.py
@@ -3,7 +3,6 @@ import json
 from json.decoder import JSONDecodeError
 from typing import Dict, List
 import os
-from time import sleep
 from flask import Flask, request, abort, jsonify, make_response
 import jsonpatch
 from flask_socketio import SocketIO, join_room
@@ -11,13 +10,12 @@ from flask_socketio import SocketIO, join_room
 from player import Player
 from game import Game
 import util
-import os
 
-CLIENT_ORIGIN = os.environ.get("CLIENT_ORIGIN", "*")
+CLIENT_ORIGIN = os.environ.get("TBG_CLIENT_ORIGIN", "*")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins=CLIENT_ORIGIN)
 
 # Handle OS interrupts
 util.register_signal_handler()
@@ -25,12 +23,15 @@ util.register_signal_handler()
 games: Dict[str, Game] = {}
 clients: Dict[str, str] = {}
 
-if 'TBG_CLIENT_ORIGIN' in os.environ:
-    CLIENT_ORIGIN = os.environ['TBG_CLIENT_ORIGIN']
-
-# Construct host and port from environment variables
 HOST = os.getenv('TBG_HOST', '0.0.0.0')
-PORT = os.getenv('TBG_PORT', 8080)
+PORT = int(os.getenv('TBG_PORT', 8080))
+
+
+# ✅ Add a root route to fix "Not Found" error
+@app.route("/")
+def home():
+    return "ThatBeanGame backend is running!"
+
 
 @socketio.on('login')
 def on_login(login_info):
@@ -46,22 +47,21 @@ def on_login(login_info):
     player.socket_sid = request.sid
     socketio.emit('client full', json.dumps(game.retrieve_game(player)), room=player.socket_sid)
 
+
 def check_valid_request(f):
-    '''Decorator. Verifies game exists and client is authorized. Returns game and client'''
     @wraps(f)
     def wrapper(*args, **kwargs):
         try:
             game: Game = games[kwargs['game_id']]
         except KeyError:
             abort(400, util.error('Game does not exist'))
-
         try:
             player: Player = [player for player in game.players if player.token == request.cookies.get('tbg_token')][0]
         except IndexError:
             abort(400, util.error('Not authorized to view game'))
-
         return f(game, player)
     return wrapper
+
 
 def update_client(game):
     for player in game.players:
@@ -70,8 +70,8 @@ def update_client(game):
         socketio.emit('client update', patch.to_string(), room=player.socket_sid)
         player.last_update = update
 
+
 def error_check(result: Dict) -> Dict:
-    '''Aborts with 400 if result is error'''
     if result.get('error'):
         abort(400, result)
     return result
@@ -84,8 +84,6 @@ def error400(err):
 
 @app.after_request
 def enable_cors(response):
-    '''Verifies server responds to all requests'''
-
     if CLIENT_ORIGIN:
         response.headers['Access-Control-Allow-Origin'] = CLIENT_ORIGIN
     response.headers['Access-Control-Allow-Credentials'] = 'true'
@@ -93,12 +91,9 @@ def enable_cors(response):
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, user-agent'
     return response
 
+
 @app.route('/api/access', methods=['GET'])
 def access() -> Dict:
-    '''
-    Checks if request has a cookie of a logged in user. If cookie exists it
-    sends user their game info. If not, it takes them to the login page.
-    '''
     cookie: str = request.cookies.get('tbg_token')
     try:
         game_id: str = clients[cookie]
@@ -106,16 +101,12 @@ def access() -> Dict:
         abort(400, util.error('Access denied'))
     if games[game_id].status == 'Completed':
         abort(400, util.error('Game already completed'))
-    player: Player = util.shrink([player for player in games[game_id].players if player.token == request.cookies.get('tbg_token')])
+    player: Player = util.shrink([player for player in games[game_id].players if player.token == cookie])
     return jsonify({'game': game_id, "player_name": player.name})
 
 
 @app.route('/api/login', methods=['POST'])
 def login() -> Dict:
-    '''
-    Adds user to the requested game as long as game isn't full or already started,
-    game exists, and user name isn't already used in game.
-    '''
     post_data: Dict = request.get_json()
     try:
         name: str = post_data['name']
@@ -123,10 +114,8 @@ def login() -> Dict:
     except KeyError:
         abort(400, util.error('Incorrect JSON data'))
 
-    # Get first public game
     game: Game = util.shrink([games[game_id] for game_id in games if games[game_id].game_type == 'public'])
 
-    # If game game_id isn't a blank string, use that game
     if game_id in games:
         game = games[game_id]
 
@@ -145,9 +134,9 @@ def login() -> Dict:
     update_client(game)
     return response
 
+
 @app.route('/api/create', methods=['POST'])
 def create_new_game():
-    '''Creates new player and game, returns game id'''
     post_data: Dict = request.get_json()
     try:
         player: Player = Player(post_data['name'])
@@ -169,20 +158,20 @@ def create_new_game():
     response.set_cookie('tbg_token', player.token, max_age=6000)
     return response
 
+
 @app.route('/api/game/<game_id>/leave', methods=['POST'])
 @check_valid_request
 def leave_game(game: Game, player: Player) -> Dict:
-    '''Player leaves game'''
     result: Dict = game.leave_game(player)
     error_check(result)
     response = make_response(jsonify(result))
     response.set_cookie('tbg_token', '', max_age=6000)
     return response
 
+
 @app.route('/api/game/<game_id>', methods=['GET'])
 @check_valid_request
 def game_status(game: Game, player: Player) -> Dict:
-    '''Returns all game info'''
     result: Dict = game.retrieve_game(player)
     error_check(result)
     return jsonify(result)
@@ -191,7 +180,6 @@ def game_status(game: Game, player: Player) -> Dict:
 @app.route('/api/game/<game_id>/start', methods=['POST'])
 @check_valid_request
 def start_game(game: Game, player: Player) -> Dict:
-    '''Starts requested game if user is host'''
     result: Dict = game.start_game(player)
     error_check(result)
     update_client(game)
@@ -201,7 +189,6 @@ def start_game(game: Game, player: Player) -> Dict:
 @app.route('/api/game/<game_id>/play/hand', methods=['POST'])
 @check_valid_request
 def play_card_from_hand(game: Game, player: Player) -> Dict:
-    '''Plays top card from hand to field specified'''
     post_data: Dict = request.get_json()
     try:
         field_index: int = post_data['field_index']
@@ -216,7 +203,6 @@ def play_card_from_hand(game: Game, player: Player) -> Dict:
 @app.route('/api/game/<game_id>/play/market', methods=['POST'])
 @check_valid_request
 def play_card_from_market(game: Game, player: Player) -> Dict:
-    '''Places card from market into field'''
     post_data: Dict = request.get_json()
     try:
         field_index: int = post_data['field_index']
@@ -228,10 +214,10 @@ def play_card_from_market(game: Game, player: Player) -> Dict:
     update_client(game)
     return jsonify(result)
 
+
 @app.route('/api/game/<game_id>/play/pending', methods=['POST'])
 @check_valid_request
 def play_card_from_pending(game: Game, player: Player) -> Dict:
-    '''Places card from market into field'''
     post_data: Dict = request.get_json()
     try:
         card_id: str = post_data['card_id']
@@ -247,7 +233,6 @@ def play_card_from_pending(game: Game, player: Player) -> Dict:
 @app.route('/api/game/<game_id>/draw/market', methods=['POST'])
 @check_valid_request
 def draw_for_market(game: Game, player: Player) -> Dict:
-    '''Draws two cards and places them in market'''
     result: Dict = game.deck_to_market(player)
     error_check(result)
     update_client(game)
@@ -257,21 +242,20 @@ def draw_for_market(game: Game, player: Player) -> Dict:
 @app.route('/api/game/<game_id>/draw/hand', methods=['POST'])
 @check_valid_request
 def draw_for_hand(game: Game, player: Player) -> Dict:
-    '''Draws three cards and places them in players hand'''
     result: Dict = game.deck_to_hand(player)
     error_check(result)
     update_client(game)
     return jsonify(result)
 
+
 @app.route('/api/game/<game_id>/trade/create', methods=['POST'])
 @check_valid_request
 def create_trade(game: Game, player: Player) -> Dict:
-    '''Creates new trade'''
     post_data: Dict = request.get_json()
     try:
         card_ids: List[str] = post_data['card_ids']
         other_player_name: str = post_data['other_player']
-        wants: List[str] = post_data['wants'] # List of card names
+        wants: List[str] = post_data['wants']
     except KeyError:
         abort(400, util.error('Incorrect JSON data'))
 
@@ -280,10 +264,10 @@ def create_trade(game: Game, player: Player) -> Dict:
     update_client(game)
     return jsonify(result)
 
+
 @app.route('/api/game/<game_id>/trade/accept', methods=['POST'])
 @check_valid_request
 def accept_trade(game: Game, player: Player) -> Dict:
-    '''Accepts a trade'''
     post_data: Dict = request.get_json()
     try:
         trade_id: str = post_data['trade_id']
@@ -295,10 +279,10 @@ def accept_trade(game: Game, player: Player) -> Dict:
     update_client(game)
     return jsonify(result)
 
+
 @app.route('/api/game/<game_id>/trade/reject', methods=['POST'])
 @check_valid_request
 def reject_trade(game: Game, player: Player) -> Dict:
-    '''Rejects a trade'''
     post_data: Dict = request.get_json()
     try:
         trade_id: str = post_data['trade_id']
@@ -309,14 +293,16 @@ def reject_trade(game: Game, player: Player) -> Dict:
     update_client(game)
     return jsonify(result)
 
+
 @app.route('/api/game/<game_id>/buy', methods=['POST'])
 @check_valid_request
 def buy_field(game: Game, player: Player) -> Dict:
-    '''Buys third field for 3 coins'''
     result = game.buy_field(player)
     error_check(result)
     update_client(game)
     return jsonify(result)
 
-print("Server starting {}:{}".format(HOST, PORT))
-socketio.run(app, HOST, PORT)
+
+if __name__ == "__main__":
+    print(f"Server starting on {HOST}:{PORT}")
+    socketio.run(app, host=HOST, port=PORT)
